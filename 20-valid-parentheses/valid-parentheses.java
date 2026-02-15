dc6a2bc34500263222b2396014fcb441fe83e4e8
Swapnil Tripathi <swapniltripathi484@gmail.com>
2026-02-15 11:03:03 +0530

    Time: 3 ms (87.48%) | Memory: 43.4 MB (57.75%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char i : s.toCharArray()){
+            if(isBracketOpen(i)){
+                stack.push(i);
+            }
+            else{
+                if(stack.size() == 0){
+                    return false;
+                }
+                else{
+                    if(areBracketMatching(stack.peek(),i)){
+                        stack.pop();
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.size()==0;
+    }
+    public boolean isBracketOpen(char c){
+        switch(c){
+            case '(':
+            case '{':
+            case '[':
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public boolean areBracketMatching(char open , char close){
+        if(open=='(' && close==')'){
+            return true;
+        }
+        else if(open=='{' && close=='}'){
+            return true;
+        }
+        else if(open=='[' && close==']'){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
