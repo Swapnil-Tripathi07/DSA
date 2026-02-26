828ffa395d6ba6211e5f577ff0bd0100f9d26a5a
Swapnil Tripathi <swapniltripathi484@gmail.com>
2026-02-26 13:18:57 +0530

    Time: 2 ms (9.07%) | Memory: 53.2 MB (7.28%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -5,14 +5,14 @@ class Solution {
         if(nums.length==1){
             target = nums[i];
         }
-        if(nums.length == 3){
-            if(nums[0]!=nums[1]){
-                target = nums[0];
-            }
-            else{
-                target = nums[2];
-            }
-        }
+        // if(nums.length == 3){
+        //     if(nums[0]!=nums[1]){
+        //         target = nums[0];
+        //     }
+        //     else{
+        //         target = nums[2];
+        //     }
+        // }
         else{
             while(i<nums.length-1){
                 if(nums[i] == nums[i+1]){
