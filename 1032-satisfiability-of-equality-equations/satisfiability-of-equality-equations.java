e7961cc9043f16f323b832163e46e3b9486b6b13
Swapnil Tripathi <swapniltripathi484@gmail.com>
2026-08-15 19:11:00 +0530

    Time: 1 ms (95.65%) | Memory: 44 MB (28.26%) - LeetSync

--- 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
@@ -1,17 +1,17 @@
 class Solution {
-    private int[] parent;
-    private int[] rank;
+    // private int[] parent;
+    // private int[] rank;
 
-    int find(int x) {
+    int find(int x,int[] parent,int[] rank) {
         if (x == parent[x]) {
             return x;
         }
-        return parent[x] = find(parent[x]); // Path compression
+        return parent[x] = find(parent[x],parent,rank); // Path compression
     }
 
-    void union(int x, int y) {
-        int xParent = find(x);
-        int yParent = find(y);
+    void union(int x, int y,int[] parent,int[] rank) {
+        int xParent = find(x,parent,rank);
+        int yParent = find(y,parent,rank);
 
         if (xParent == yParent) {
             return;
@@ -29,8 +29,8 @@ class Solution {
     }
 
     public boolean equationsPossible(String[] equations) {
-        parent = new int[26];
-        rank = new int[26];
+        int[] parent = new int[26];
+        int[] rank = new int[26];
 
         for(int i = 0 ; i<26 ; i++){
             parent[i] = i;
@@ -40,7 +40,7 @@ class Solution {
         //First do union of equal chars.
         for(String s : equations){ 
             if(s.charAt(1) == '='){    // "a==b"
-                union(s.charAt(0)-'a',s.charAt(3)-'a');
+                union(s.charAt(0)-'a',s.charAt(3)-'a',parent,rank);
             }
         }
 
@@ -50,8 +50,8 @@ class Solution {
                 char first = s.charAt(0);  //a
                 char second = s.charAt(3); //b
 
-                int parent_first = find(first-'a');
-                int parent_second = find(second-'a');
+                int parent_first = find(first-'a',parent,rank);
+                int parent_second = find(second-'a',parent,rank);
 
                 if(parent_first == parent_second){
                     return false;
