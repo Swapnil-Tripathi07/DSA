151dafccc5f044d983c88dae3b19d1f1f21c1557
Swapnil Tripathi <swapniltripathi484@gmail.com>
2026-08-20 09:51:32 +0530

    Time: 1 ms (95.69%) | Memory: 43.9 MB (11.89%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,15 +1,32 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int i = 0, j = 0;
+        int i  = 0;
+        int j = 0;
+        return subsequence(s,t,i,j);
+    }
 
-        while (i < s.length() && j < t.length()) {
-            if (s.charAt(i) == t.charAt(j)) {
-                i++;
-            }
-            j++;
+    public boolean subsequence(String s , String t,int i , int j){
+        if(i == s.length()){
+            return true;
         }
+        if(i>s.length()-1 || j>t.length()-1){
+            return false;
+        }
+        
 
-        // If i reached end of s, all characters were matched
-        return i == s.length();
+        if(s.charAt(i) == t.charAt(j)){
+            i++;
+            j++;
+            return subsequence(s,t,i,j);
+        }
+        else{
+            j++;
+            return subsequence(s,t,i,j);
+        }
+        
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
