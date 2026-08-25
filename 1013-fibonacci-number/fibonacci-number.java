af1b34ea17e0fe4e08e07fbc839cc4ac863aa2e1
Swapnil Tripathi <swapniltripathi484@gmail.com>
2026-08-25 09:17:48 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (52.58%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,12 +1,25 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1){
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+
+        return fibonacci(n,dp);
+    }
+
+    public int fibonacci(int n , int[] dp){
+        if(n==0 || n==1){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        dp[n] = fibonacci(n-1,dp)+fibonacci(n-2,dp);
+        return dp[n];
     }
 }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
