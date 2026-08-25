e18eaf82c32ac7b9a2ba473d1637c48d51b3e077
Swapnil Tripathi <swapniltripathi484@gmail.com>
2026-08-25 11:29:04 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (54.83%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,29 +1,47 @@
 class Solution {
 
     // DP SOLUTION
-    public int climbStairs(int n) {
+    // public int climbStairs(int n) {
     
-       int[] dp = new int[n+1];
-       Arrays.fill(dp,-1);
-       return climb(n,0,dp);
+    //    int[] dp = new int[n+1];
+    //    Arrays.fill(dp,-1);
+    //    return climb(n,0,dp);
         
+    // }
+
+    // private int climb(int n , int count,int[] dp){
+    //     if(count ==  n){
+    //         return 1;
+    //     }
+    //     if(count > n){
+    //         return 0;
+    //     }
+    //     if(dp[count] != -1){
+    //         return dp[count];
+    //     }
+    //     int oneStep = climb(n,count+1,dp);
+    //     int twoStep = climb(n,count+2,dp);
+    //     dp[count] = oneStep+twoStep;
+    //     return dp[count];
+    // }
+
+    //TABULATION
+    public int climbStairs(int n){
+        if(n<=2){
+            return n;
+        }
+
+        int[] tabulation = new int[n+1];
+        tabulation[0] = 0;
+        tabulation[1] = 1;
+        tabulation[2] = 2;
+        for(int i = 3 ; i<=n ; i++){
+            tabulation[i] = tabulation[i-1] + tabulation[i-2]; 
+        }
+        return tabulation[n];
+
     }
 
-    private int climb(int n , int count,int[] dp){
-        if(count ==  n){
-            return 1;
-        }
-        if(count > n){
-            return 0;
-        }
-        if(dp[count] != -1){
-            return dp[count];
-        }
-        int oneStep = climb(n,count+1,dp);
-        int twoStep = climb(n,count+2,dp);
-        dp[count] = oneStep+twoStep;
-        return dp[count];
-    }
 }
 
 // Synced seamlessly with LeetHub Pro
