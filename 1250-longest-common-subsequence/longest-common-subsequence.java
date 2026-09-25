13d51ac01d6cc8f5c6fcf1f1a4b234c36f0a7ec7
Swapnil Tripathi <swapniltripathi484@gmail.com>
2026-09-25 18:59:44 +0530

    Time: 21 ms (54.68%) | Memory: 53.9 MB (52.57%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,35 +1,54 @@
 class Solution {
+    public int longestCommonSubsequence(String str1, String str2) {
+        char[] s1 = str1.toCharArray();
+        char[] s2 = str2.toCharArray();
+        int[][] dp = new int[s1.length][s2.length];
+        for(int i = 0 ; i<s1.length ; i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return lCSDP(s1,s2,0,0,dp);
+    }
 
-    public int longestCommonSubsequence(String text1, String text2) {
 
-        int n = text1.length();
-        int m = text2.length();
 
-        int[][] dp = new int[n + 1][m + 1];
-
-        for (int i = 1; i <= n; i++) {
-
-            for (int j = 1; j <= m; j++) {
-
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                }
-
-                else {
-
-                    dp[i][j] = Math.max(
-                            dp[i - 1][j],
-                            dp[i][j - 1]
-                    );
-                }
-            }
+    //strarts from 0 index.
+    public static int lCS(String s1 , String s2){
+        if(s1.length()==0 || s2.length()==0){
+            return 0;
         }
 
-        return dp[n][m];
+        if(s1.charAt(0) == s2.charAt(0)){
+            return 1 + lCS(s1.substring(1,s1.length())   ,    s2.substring(1,s2.length()));
+        }
+
+        int one = lCS(s1.substring(1,s1.length()) , s2);
+        int two = lCS(s1,s2.substring(1,s2.length()));
+        return Math.max(one,two);
+    }
+
+
+
+    //DP Approach
+    public static int lCSDP(char[] s1 , char[] s2 , int i , int j , int[][] dp){
+        if(i>s1.length-1 || j>s2.length-1){
+            return 0;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        if(s1[i] == s2[j]){
+            return 1 + lCSDP(s1,s2,i+1,j+1,dp);
+        }
+
+        int one = lCSDP(s1,s2,i+1,j,dp);
+        int two = lCSDP(s1,s2,i,j+1,dp);
+        dp[i][j] = Math.max(one,two);
+        return dp[i][j];
     }
 }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
